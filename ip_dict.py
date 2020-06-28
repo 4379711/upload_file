0c6e8f050278f8b5a043d0e2ae66202876d4efae
4379711 <4379711@qq.com>
2020-06-28 18:08:19 +0800

    动态添加hosts

--- ip_dict.py
+++ ip_dict.py
@@ -1,13 +1,29 @@
 # -*- coding:utf-8 -*-
+import os
+import re
 
+clear_hosts = re.compile("\n| |,|")
 DEFAULT_PATH = r"C:\file_uploads"
 DEFAULT_PATH_dict = {"us": DEFAULT_PATH, "uk": DEFAULT_PATH, "jp": DEFAULT_PATH}
 
+# 特殊的ip/域名,指定特殊的路径,就放在这里
 special_path = {
     # "127.0.0.1": DEFAULT_PATH_dict,
 }
 
-all_hosts_by_default_path = {"elecsport.amzbsr.top", 'b', 'c'}
 
-ips_dict = {}.fromkeys(all_hosts_by_default_path, DEFAULT_PATH_dict)
+# 清理文件中的特殊符号
+def replace_(host_str):
+    tmp = clear_hosts.sub('', host_str)
+    return tmp
+
+
+# 读取hosts.txt中配置的域名
+file_host = []
+if os.path.exists("hosts.txt"):
+    with open("hosts.txt", 'r') as f:
+        file_host = f.readlines()
+
+ips_dict = {}.fromkeys(map(replace_, file_host), DEFAULT_PATH_dict)
 ips_dict.update(special_path)
+# print(ips_dict)
